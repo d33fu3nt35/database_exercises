826dd155286e61a0a5ad31d6d48edde5afd58145
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-05-31 13:43:23 -0500

    Finished INSERTS Exercises

--- basic_statements/albums_seeder.sql
+++ basic_statements/albums_seeder.sql
@@ -1,5 +1,26 @@
 
 USE codeup_test_db;
 
-INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('Michael Jackson', 'Thriller', 1982, 46.3, 'Pop, Rock, R&B');
-INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('AC/DC', 'Back in Black', 1980, 26.1, 'Hard rock');
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 46.3, 'Pop, Rock, R&B');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('AC/DC', 'Back in Black', 1980, 26.1, 'Hard rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+  VALUE ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.2, 'Progressive rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+  VALUE ('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'R&B, Soul, Pop, Soundtrack');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+  VALUE ('Meat Loaf', 'Bat Out of Hell', 1977, 21.5, 'Hard rock, Progressive rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+  VALUE ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 32.2, 'Rock, Soft rock, Folk rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+  VALUE ('Bee Gees', 'Saturday Night Fever', 1977, 20.6, 'Disco');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+  VALUE ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock');
