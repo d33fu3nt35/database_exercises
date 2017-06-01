7f2360a035abf4bee399a7a81eb21f8e5868366b
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-06-01 15:36:03 -0500

    Initial Where Continued Exercise Completed.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,17 +1,68 @@
-SELECT emp_no, first_name, last_name
-FROM employees
-WHERE last_name IN ('Irena', 'Vidya', 'Maya');
+USE employees;
 
-SELECT emp_no, first_name, last_name
+# Now add a condition to find everybody with those names who is also male — 441 rows.
+SELECT first_name, last_name, gender
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE gender = 'M'
+      AND (
+        first_name = 'Irena'
+        OR first_name = 'Vidya'
+        OR first_name = 'Maya'
+      );
+
+# Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%E';
+
+# Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+# Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E';
 
 SELECT hire_date, first_name, last_name
 FROM employees
-WHERE hire_date BETWEEN 1990 AND 1999;
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
-SELECT * FROM employees WHERE birth_date LIKE '%12-25%';
+# Find all employees hired in the 90s and born on Christmas — 362 rows.
+SELECT hire_date, birth_date, first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%12-25%';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%12-25%';
 
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE '%Q%';
+
+# Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE '%Q%'
+AND last_name NOT LIKE '%QU%';
+
+SELECT *
+FROM employees
+WHERE first_name = 'Carl'
+OR first_name = 'Karl';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE emp_no < 20000
+      AND
+        last_name IN ('Herber','Baek')
+  OR first_name = 'Shridhar';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE emp_no < 20000
+      AND (
+        last_name IN ('Herber', 'Baek')
+        OR first_name = 'Shridhar'
+);
