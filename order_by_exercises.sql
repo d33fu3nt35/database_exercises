b220ec5eed1a609f420e5a5b86ae3300716c7ce2
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-06-01 16:14:34 -0500

    Order By Exercise Finished W/ Comments For Users!

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,6 +1,8 @@
 USE employees;
 
-# Now add a condition to find everybody with those names who is also male — 441 rows.
+# Modify your first query to order by first name. The first result should be Irena Majewski and the last result should be Vidya Schaft.
+# Update the query to order by first name and then last name. The first result should now be Irena Acton and the last should be Vidya Zweizig.
+# Change the order by clause so that you order by last name before first name. Your first result should still be Irena Acton but now the last result should be Maya Zyda.
 SELECT first_name, last_name, gender
 FROM employees
 WHERE gender = 'M'
@@ -8,13 +10,16 @@ WHERE gender = 'M'
         first_name = 'Irena'
         OR first_name = 'Vidya'
         OR first_name = 'Maya'
-      );
+      )
+ORDER BY last_name, first_name;
 
-# Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+# Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
+# Now reverse the sort order for both queries.
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%'
-      OR last_name LIKE '%E';
+      OR last_name LIKE '%E'
+ORDER BY emp_no DESC;
 
 # Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
 SELECT first_name, last_name
@@ -27,11 +32,12 @@ SELECT hire_date, first_name, last_name
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
-# Find all employees hired in the 90s and born on Christmas — 362 rows.
+# Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
 SELECT hire_date, birth_date, first_name, last_name
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-      AND birth_date LIKE '%12-25%';
+      AND birth_date LIKE '%12-25%'
+ORDER BY hire_date DESC ;
 
 # Employees born on Christmas — 842 rows.
 SELECT *
