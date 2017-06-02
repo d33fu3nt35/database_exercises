5966442e38b79df6dfc3dfad31d2313f43f7e806
Daniel De La Fuentes <d33fu3nt35@gmail.com>
2017-06-02 12:08:33 -0500

    LIMIT Exercises Finished W/ Comments!

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -12,5 +12,17 @@ SELECT hire_date, birth_date, first_name, last_name
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
       AND birth_date LIKE '%12-25%'
-ORDER BY hire_date DESC
+ORDER BY birth_date ASC , hire_date DESC
 LIMIT 5 OFFSET 50;
+
+SELECT CONCAT('Hello ', 'World!');
+
+SELECT NOW();
+SELECT CURDATE();
+SELECT CURTIME();
+
+SELECT CONCAT(
+    'Learning To Code For ',
+    (UNIX_TIMESTAMP() - UNIX_TIMESTAMP('2017-03-27')) / (60 * 60 * 24 * 7),
+    ' Weeks.'
+);
